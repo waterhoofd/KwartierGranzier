661e7c7aa8cf3f66588a8350d868df6c5e67938f
waterhoofd <38175270+waterhoofd@users.noreply.github.com>
2020-07-25 18:02:37 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,71 @@
+// Particles uitzetten
+const doParticles = true;
+
+
+
+
+
+
+function getWidth() { 
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
+}
+if (doParticles) {
+	if (getWidth() < 400) $.firefly({minPixel: 1,maxPixel: 2,total: 20});
+	else $.firefly({minPixel: 1,maxPixel: 3,total: 40});
+}
+
+// Click copy
+let t;
+$(document).ready(()=>{
+	t = $(".ip").html();
+})
+$(document).on("click",".ip",()=>{
+	let copy = document.createElement("textarea");
+	copy.style.position = "absolute";
+	copy.style.left = "-99999px";
+	copy.style.top = "0";
+	copy.setAttribute("id", "ta");
+	document.body.appendChild(copy);
+	copy.textContent = t;
+	copy.select();
+	document.execCommand("copy");
+	$(".ip").html("<span class='extrapad'>IP gekopieerd!</span>");
+	setTimeout(function(){
+		$(".ip").html(t);
+		var copy = document.getElementById("ta");
+		copy.parentNode.removeChild(copy);
+	},800);
+});
+
+// This is to fetch the player count
+$(document).ready(()=>{
+  const ip = $(".sip").attr("data-ip");
+  const port = $(".sip").attr("data-port");
+
+  $.get(`https://mcapi.us/server/status?ip=${ip}&port=${port}`, (result)=>{
+    if (result.online) {
+      $(".sip").html(result.players.now);
+    } else {
+      $(".playercount").html("Server is offline!");
+    }
+  });
+
+  setInterval(()=>{
+    $.get(`https://mcapi.us/server/status?ip=${ip}&port=${port}`, (result)=>{
+      if (result.online) {
+        $(".sip").html(result.players.now);
+      } else {
+        $(".playercount").html("Server is offline!");
+      }
+    });
+  }, 3000);
+});
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.getElementById('myImage').setAttribute('draggable', false);
